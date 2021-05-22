10cc87a24e4b0193e49ad534b622acd379c43d59
toughnoah <toughnoah@163.com>
2021-05-22 16:34:17 +0800

    optimize coverage

--- api/v1alpha1/zz_generated.deepcopy_test.go
+++ api/v1alpha1/zz_generated.deepcopy_test.go
@@ -2,6 +2,7 @@ package v1alpha1
 
 import (
 	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 )
@@ -21,5 +22,71 @@ func TestElastalert_DeepCopy(t *testing.T) {
 		},
 	}
 	newea := ea.DeepCopy()
+	assert.Equal(t, newea, ea)
+}
+
+func TestElastalertSpec_DeepCopy(t *testing.T) {
+	ea := &ElastalertSpec{
+		PodTemplateSpec: v1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+		},
+		Rule: []FreeForm{
+			NewFreeForm(map[string]interface{}{
+				"test": true,
+			}),
+		},
+	}
+	newea := ea.DeepCopy()
+	assert.Equal(t, newea, ea)
+}
+
+func TestElastalertStatus_DeepCopy(t *testing.T) {
+	ea := &ElastalertStatus{
+		Version: "v1.0",
+		Phase:   "RUNNIG",
+	}
+	newea := ea.DeepCopy()
+	assert.Equal(t, newea, ea)
+}
+
+func TestElastalertList_DeepCopy(t *testing.T) {
+	ea := &ElastalertList{
+		Items: []Elastalert{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+			},
+		},
+	}
+	newea := ea.DeepCopy()
+	assert.Equal(t, newea, ea)
+}
+
+func TestFreeForm_DeepCopyInTo(t *testing.T) {
+	nf := NewFreeForm(map[string]interface{}{
+		"test": 1,
+	})
+	newnf := new(FreeForm)
+	nf.DeepCopyInto(newnf)
+	assert.Equal(t, *newnf, nf)
+}
+func TestElastalert_DeepCopyInTo(t *testing.T) {
+	ea := &Elastalert{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test",
+		},
+		Status: ElastalertStatus{
+			Condictions: []metav1.Condition{
+				{
+					Type: "test",
+				},
+			},
+		},
+	}
+	newea := new(Elastalert)
+	ea.DeepCopyInto(newea)
 	assert.Equal(t, newea, ea)
 }
