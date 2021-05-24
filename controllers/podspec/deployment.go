07d5f56fc3c7bc6141c399f35def9818ae3aeac6
toughnoah <toughnoah@163.com>
2021-05-25 01:03:03 +0800

    try e2e test

--- controllers/podspec/deployment.go
+++ controllers/podspec/deployment.go
@@ -3,6 +3,7 @@ package podspec
 import (
 	"context"
 	"elastalert/api/v1alpha1"
+	"fmt"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
@@ -80,9 +81,14 @@ func WaitForStability(c client.Client, ctx context.Context, dep appsv1.Deploymen
 			return false, err
 		}
 		seen = true
+		fmt.Println(d.Status.ReadyReplicas)
 		if d.Status.ReadyReplicas != d.Status.Replicas {
 			//"Deployment has not stabilized yet"
 			return false, nil
+		}
+		for i := 0; i < 10; i++ {
+			fmt.Println(d.Status.ReadyReplicas)
+			time.Sleep(time.Second * 2)
 		}
 
 		//"Deployment has stabilized"
