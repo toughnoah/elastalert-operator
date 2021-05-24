46ca5d71891c42fd2bc73be5f387b847765d98d1
toughnoah <toughnoah@163.com>
2021-05-25 00:01:14 +0800

    try e2e test

--- controllers/podspec/deployment.go
+++ controllers/podspec/deployment.go
@@ -81,9 +81,10 @@ func WaitForStability(c client.Client, ctx context.Context, dep appsv1.Deploymen
 			return false, err
 		}
 		seen = true
+		fmt.Println(d.Status.ReadyReplicas)
+		fmt.Println(*dep.Spec.Replicas)
+		fmt.Println(dep.ObjectMeta.Name)
 		if d.Status.ReadyReplicas != *dep.Spec.Replicas {
-			fmt.Println(d.Status.ReadyReplicas)
-			fmt.Println(*dep.Spec.Replicas)
 			fmt.Println("go here")
 			//"Deployment has not stabilized yet"
 			return false, nil
