7d07d101573fa809645ab79db4d4f1d0be5c16b8
toughnoah <toughnoah@163.com>
2021-05-25 10:35:39 +0800

    try e2e test

--- controllers/podspec/deployment.go
+++ controllers/podspec/deployment.go
@@ -3,7 +3,6 @@ package podspec
 import (
 	"context"
 	"elastalert/api/v1alpha1"
-	"fmt"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
@@ -63,31 +62,35 @@ func BuildDeployment(elastalert v1alpha1.Elastalert) (*appsv1.Deployment, error)
 func WaitForStability(c client.Client, ctx context.Context, dep appsv1.Deployment) error {
 	// the images, subsequent runs should take only a few seconds
 	seen := false
-	return wait.Poll(5*time.Second, 3*time.Minute, func() (done bool, err error) {
-		d := &appsv1.Deployment{}
-		if err := c.Get(ctx, types.NamespacedName{Name: dep.Name, Namespace: dep.Namespace}, d); err != nil {
-			if k8serrors.IsNotFound(err) {
-				if seen {
-					// we have seen this object before, but it doesn't exist anymore!
-					// we don't have anything else to do here, break the poll
-					//"Deployment has been removed."
-					return true, err
+	return wait.Poll(time.Second, 3*time.Minute,
+		func() (done bool, err error) {
+			d := &appsv1.Deployment{}
+			if err := c.Get(ctx, types.NamespacedName{Name: dep.Name, Namespace: dep.Namespace}, d); err != nil {
+				if k8serrors.IsNotFound(err) {
+					if seen {
+						// we have seen this object before, but it doesn't exist anymore!
+						// we don't have anything else to do here, break the poll
+						//"Deployment has been removed."
+						return true, err
+					}
+
+					// the object might have not been created yet
+					//"Deployment doesn't exist yet."
+					return false, nil
 				}
-
-				// the object might have not been created yet
-				//"Deployment doesn't exist yet."
-				return false, nil
+				return false, err
 			}
-			return false, err
-		}
-		seen = true
-		fmt.Println(d.Status.AvailableReplicas)
-		if d.Status.AvailableReplicas != *d.Spec.Replicas {
-			//"Deployment has not stabilized yet"
-			return false, nil
-		}
-
-		//"Deployment has stabilized"
-		return true, nil
-	})
+			seen = true
+			var res bool
+			for i := 0; i < 3; i++ {
+				time.Sleep(time.Second * 2)
+				res = true
+				if d.Status.AvailableReplicas != *d.Spec.Replicas {
+					//"Deployment has not stabilized yet"
+					res = false
+				}
+			}
+			//"Deployment has stabilized"
+			return res, nil
+		})
 }
