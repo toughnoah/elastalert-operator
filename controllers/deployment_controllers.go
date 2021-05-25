9e65f6d27dfec752527a448dc7b29f4ebf8aa60e
toughnoah <toughnoah@163.com>
2021-05-25 17:48:02 +0800

    fix unittest

--- controllers/deployment_controllers.go
+++ controllers/deployment_controllers.go
@@ -11,6 +11,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
