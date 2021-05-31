f358c50971a8ff33fde01a8ca04a9fe6f92821d0
toughnoah <toughnoah@163.com>
2021-05-31 16:58:17 +0800

    fix e2e test cases

--- controllers/e2e/deploy_test.go
+++ controllers/e2e/deploy_test.go
@@ -3,7 +3,6 @@ package e2e
 import (
 	"context"
 	"elastalert/api/v1alpha1"
-	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"gopkg.in/yaml.v2"
@@ -198,9 +197,6 @@ var _ = Describe("Elastalert Controller", func() {
 })
 
 func compare(source string, dest map[string]interface{}) bool {
-	var data = make(map[string]interface{})
-	_ = yaml.Unmarshal([]byte(source), &data)
-	fmt.Println(data)
-	fmt.Println(dest)
-	return reflect.DeepEqual(data, dest)
+	out, _ := yaml.Marshal(dest)
+	return reflect.DeepEqual([]byte(source), out)
 }
